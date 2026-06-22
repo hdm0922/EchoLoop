2d55f7bb346fa051acf46894ff129585d737af8e
hdm0922 <hdm0922@gmail.com>
2026-06-22 20:54:28 +0900

    Create Loop Subsystem

---
+++ Source/EchoLoop/EchoLoopSubsystem.cpp
@@ -0,0 +1,135 @@
+
+
+#include "EchoLoopSubsystem.h"
+
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/PlayerStart.h"
+
+#include "Kismet/GameplayStatics.h"
+#include "TimerManager.h"
+
+void UEchoLoopSubsystem::OnWorldBeginPlay(UWorld& InWorld)
+{
+	Super::OnWorldBeginPlay(InWorld);
+
+	if (!IsValid(&InWorld)) return;
+
+	// Get weak reference : Player Start
+	{
+		AActor* pPlayerStart = UGameplayStatics::GetActorOfClass(&InWorld, APlayerStart::StaticClass());
+		this->PlayerStart = Cast<APlayerStart>(pPlayerStart);
+	}
+
+	// Get weak reference : Echo Record Component
+	UE_LOG(LogTemp, Warning, TEXT("Get Echo Record Component WeakPtr ... "));
+
+
+	this->ResetEntityStatus();
+	this->StartLoopTimer();
+
+	return;
+}
+
+void UEchoLoopSubsystem::Deinitialize()
+{
+
+	if (UWorld* World = this->GetWorld())
+	{
+		World->GetTimerManager().ClearTimer(this->LoopTimer);
+	}
+
+	Super::Deinitialize();
+
+	return;
+}
+
+void UEchoLoopSubsystem::Callback_TimeLimitExceeded()
+{
+	if (!IsValid(this->GetWorld())) return;
+
+	this->RecordEcho();
+	this->ResetEntityStatus();
+	this->StartLoopTimer();
+	
+	return;
+}
+
+void UEchoLoopSubsystem::RecordEcho()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Save Recorded Echo ..."));
+
+
+
+	return;
+}
+
+void UEchoLoopSubsystem::ResetCharacterStatus(ACharacter* InCharacter)
+{
+	if (!IsValid(InCharacter)) return;
+
+
+	InCharacter->StopJumping();
+
+	// Reset Movement Component
+	if (UCharacterMovementComponent* MovementComponent = InCharacter->GetCharacterMovement())
+	{
+		MovementComponent->StopMovementImmediately();
+		MovementComponent->SetMovementMode(MOVE_Walking);
+	}
+
+	// Reset Transform & Reset Camera
+	if (APlayerStart* pPlayerStart = this->PlayerStart.Get())
+	{
+		const FTransform& StartTransform = pPlayerStart->GetActorTransform();
+
+		InCharacter->SetActorLocationAndRotation(
+			StartTransform.GetLocation(),
+			StartTransform.Rotator(),
+			false, 
+			nullptr,
+			ETeleportType::TeleportPhysics
+		);
+
+		if (AController* Controller = InCharacter->GetController())
+		{
+			Controller->SetControlRotation(StartTransform.Rotator());
+		}
+	}
+
+
+	return;
+}
+
+void UEchoLoopSubsystem::ResetEntityStatus()
+{
+	// Get player
+	ACharacter* PlayerCharacter = nullptr;
+	{
+		APlayerController*	PlayerController	= this->GetWorld()->GetFirstPlayerController();
+		APawn*				PlayerPawn			= PlayerController ? PlayerController->GetPawn() : nullptr;
+		PlayerCharacter							= PlayerPawn ? Cast<ACharacter>(PlayerPawn) : nullptr;
+	}
+	
+	// Reset Status : Player
+	this->ResetCharacterStatus(PlayerCharacter);
+
+	// Reset Status : Echo
+	UE_LOG(LogTemp, Warning, TEXT("Reset Echo ... "));
+
+	return;
+}
+
+void UEchoLoopSubsystem::StartLoopTimer()
+{
+
+	this->GetWorld()->GetTimerManager().SetTimer
+	(
+		this->LoopTimer,
+		this,
+		&UEchoLoopSubsystem::Callback_TimeLimitExceeded,
+		UEchoLoopSubsystem::LOOP_DURATION
+	);
+
+	return;
+}
---
+++ Source/EchoLoop/EchoLoopSubsystem.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/WorldSubsystem.h"
+#include "EchoLoopSubsystem.generated.h"
+
+UCLASS()
+class ECHOLOOP_API UEchoLoopSubsystem : public UWorldSubsystem
+{
+	GENERATED_BODY()
+	
+protected:
+
+	virtual void OnWorldBeginPlay(UWorld& InWorld) override;
+	virtual void Deinitialize() override;
+
+private:
+
+	void Callback_TimeLimitExceeded();
+
+	void RecordEcho();
+	void ResetCharacterStatus(class ACharacter* InCharacter);
+	void ResetEntityStatus();
+
+	void StartLoopTimer();
+
+
+	static constexpr float LOOP_DURATION = 10.0f;
+
+	FTimerHandle LoopTimer;
+
+	TArray<class FEchoRecord*> EchoRecordArray;
+
+	TWeakObjectPtr<class UEchoRecordComponent> EchoRecorder;
+	TWeakObjectPtr<class APlayerStart> PlayerStart;
+};
